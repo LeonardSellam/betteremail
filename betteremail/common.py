10e2dc6442316b3a89f3363394d73659f5b251e3
Leonard Sellam <leonardsellam@hey.com>
2023-10-12 00:12:09 +0200

    change default scopes

--- betteremail/common.py
+++ betteremail/common.py
@@ -1,6 +1,6 @@
 import os
 
-MICROSOFT_AUTH_URL = os.getenv("MICROSOFT_AUTH_URL", "https://login.microsoftonline.com/consumers/oauth2/authorize")
+MICROSOFT_AUTH_URL = os.getenv("MICROSOFT_AUTH_URL", "https://login.live.com/oauth20_authorize.srf")
 GMAIL_AUTH_URL = os.getenv("GMAIL_AUTH_URL", "https://accounts.google.com/o/oauth2/auth")
 
 MICROSOFT_CLIENT_ID = os.getenv("MICROSOFT_CLIENT_ID", "be73bf81-df80-40e2-baf7-9f06cec51885")
@@ -11,7 +11,7 @@ REDIRECT_URI = os.getenv("REDIRECT_URI", "http://localhost:8000/callback")
 GMAIL_CLIENT_ID = os.getenv("GMAIL_CLIENT_ID", "")
 GMAIL_CLIENT_SECRET = os.getenv("GMAIL_CLIENT_SECRET", "")
 
-MICROSOFT_CLIENT_SCOPES = os.getenv("MICROSOFT_CLIENT_SCOPES", "User.Read Mail.Read email offline_access openid profile")
+MICROSOFT_CLIENT_SCOPES = os.getenv("MICROSOFT_CLIENT_SCOPES", "https://graph.microsoft.com/.default")
 MICROSOFT_API_SCOPES = os.getenv("MICROSOFT_API_SCOPES", "User.Read Mail.Read email")
 
 DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
